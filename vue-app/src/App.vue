2af32d19c483c150451473e20a4ee2d9f10d5051
Shyniben <33348633+shyniben@users.noreply.github.com>
2020-09-23 12:26:56 +0400

    setup computed, methods, watch

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,12 +2,56 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/home-old">Homeold</router-link>
+      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/home-old">Homeold</router-link> -->
     </div>
     <router-view />
+    <h3>@root :{{ user.username }} - {{ fullname }}</h3>
+    <strong>Followers: {{ followers }}</strong>
+    <button @click="followUser">Follow</button>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "Shyniben",
+        firstname: "Shyniben",
+        lastname: "Koyakkil",
+        email: "shyniben@gmail.com",
+        isAdmin: true
+      }
+    };
+  },
+  watch: {
+    followers(newFollowCount, oldFollowCount) {
+      if (oldFollowCount < newFollowCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+  computed: {
+    fullname() {
+      return `${this.user.firstname} ${this.user.lastname}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+};
+</script>
+
 
 <style>
 #app {
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
     <h1>This is Home page</h1>
+    <h3>@@@@</h3>
+    <!-- <h3>@home: {{ user.username }}-- {{ fullname }}</h3> -->
   </div>
 </template>
+
+
